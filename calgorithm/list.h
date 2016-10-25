dfc2c05bfdb918cd1f693d7470c779a611f583d6
tanghailu <tanghailu@fz.com>
2016-10-25 18:22:17 +0800

    ignore IDE files

---
+++ calgorithm/list.h
@@ -0,0 +1,40 @@
+/* lsit.h */
+
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdlib.h>
+
+/* Define a structure for linked list element. */
+typedef struct ListElmt_
+{
+	void *data;
+	struct ListElmt_ *next;
+} ListElmt;
+
+/* Define a structure for linked lists. */
+typedef struct List_
+{
+	int size;
+	int (*match)(const void *key1, const void *key2);
+	void (*destory)(void *data);
+
+	ListElmt *head;
+	ListElmt *tail;
+} List;
+
+/* Public Interface */
+void list_init(List *list, void (*destory)(void *data));
+void list_destory(List *list);
+void list_ins_next(List *list, ListElmt *element, const void *data);
+void list_rem_next(List *list, ListElmt *element, void **data);
+
+#define list_size(list) ((list)->size)
+#define list_head(list) ((list)->head)
+#define list_tail(list) ((list)->tail)
+#define list_is_head(list, element) ((element) == (list)->head ? 1 : 0)
+#define list_is_tail(element) ((element)->next == NULL ? 1 : 0)
+#define list_data(element) ((element)->data)
+#define list_next(element) ((element)->next)
+
+#endif
