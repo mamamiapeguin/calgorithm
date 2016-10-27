ffd3c2d3a1459f73eea88417eb321ed395ca6d55
tanghailu <tanghailu@fz.com>
2016-10-27 14:34:23 +0800

    stack & queue

---
+++ calgorithm/stack.h
@@ -0,0 +1,26 @@
+#ifndef STACK_H
+#define STACK_H
+
+#endif //STACK_H
+
+#include <stdlib.h>
+
+#include "list.h"
+
+/* Implement stacks as linked lists. */
+typedef List Stack;
+
+/* Public Interface */
+#define stack_init list_init
+
+#define stack_destroy list_destroy
+
+int stach_push(Stack *stack, const void *data);
+
+int stack_pop(Stack *stack, void **data);
+
+#define stack_peek(stack) ((stack)->head == NULL ? NULL : (stack)->head->data)
+
+#define stack_size list_size
+
+#endif
